b008416d93613d45ab69758d4c5765878a353c4d
Purity Wambua <puritywamb@gmail.com>
2024-09-02 21:21:46 +0300

    oop putting together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,57 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
+# class Book:
+#     def __init__(self, title, page_count):
+#         self.title = title
+#         self.page_count = page_count  # Use the setter to validate and set page_count
 
+#     @property
+#     def page_count(self):
+#         return self._page_count
+
+#     @page_count.setter
+#     def page_count(self, value):
+#         if not isinstance(value, int):
+#             print("page_count must be an integer")
+#             raise ValueError("page_count must be an integer")
+#         self._page_count = value
+
+#     def turn_page(self):
+#         print("Flipping the page...wow, you read fast!")
+# class Book:
+#     def __init__(self, title, page_count):
+#         self.title = title
+#         self.page_count = page_count  # Use the setter to validate and set page_count
+
+#     @property
+#     def page_count(self):
+#         return self._page_count
+
+#     @page_count.setter
+#     def page_count(self, value):
+#         if not isinstance(value, int):
+#             print("page_count must be an integer")
+#             raise ValueError("page_count must be an integer")
+#         self._page_count = value
+
+#     def turn_page(self):
+#         print("Flipping the page...wow, you read fast!")
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Initialize private variable for page count
+        self.page_count = page_count  # Use the setter to validate and set page count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            # no exception raised if you want to pass the test checking for printed output
+            return
+        self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
-#!/usr/bin/env python3
-
+# # #!/usr/bin/env python3
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Initialize private variable for size
+        self.size = size  # Use the setter to validate and set size
+        self.condition = 'New'  # Default condition for a new shoe
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            # no exception raised if you want to pass the test checking for printed output
+            return
+        self._size = value
+
+    def cobble(self):
+        self.condition = 'New'
+        print("Your shoe is as good as new!")
